32bdb9b1fe7c0345fe79949f6a0b8751896bc99b
alexey4uk <alexey4uk@gmail.com>
2025-11-27 08:49:58 +0300

    Добавлен favicon

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }} - Вход</title>
+    <link rel="icon" href="{{ Vite::asset('resources/images/favicon.svg') }}">
     <!-- Assets -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Мастерская CRM')</title>
+    <link rel="icon" href="{{ Vite::asset('resources/images/favicon.svg') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="h-full bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
