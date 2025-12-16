d3a3f3377462f85988ea48c9bd5d3a4d3e21f067
alex <alexey4uk@gmail.com>
2025-12-16 08:28:27 +0300

    Edit controllers

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,18 +7,20 @@ use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    //
     public function index()
     {
-        return view('dashboard', ['clientCount' => $this->clientCount()]);
+        $clientCount = $this->clientCount();
+
+        return view('dashboard', compact('clientCount'));
     }
 
-    public function clientCount()
+    public function clientCount(): int
     {
         $clientCount = 0;
+        $businesses = Auth::user()->businesses()->with('clients')->get();
 
-        foreach (auth()->user()->businesses()->get() as $business) {
-            $clientCount = $clientCount + $business->clients->count();
+        foreach ($businesses as $business) {
+            $clientCount += $business->clients->count();
         }
 
         return $clientCount;
--- app/Http/Middleware/EnsureUserOnboarded.php
+++ app/Http/Middleware/EnsureUserOnboarded.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
 class EnsureUserOnboarded
@@ -15,7 +16,7 @@ class EnsureUserOnboarded
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (auth()->user()->businesses->isNotEmpty()) {
+        if (Auth::user()->businesses->isNotEmpty()) {
             return $next($request);
         }
 
