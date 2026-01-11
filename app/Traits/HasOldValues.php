c3691353fdc20d29b38748ed4d1ea18edf3f83fe
alex <alexey4uk@gmail.com>
2026-01-11 23:00:36 +0300

    Feat: add trait HasOldValues

---
+++ app/Traits/HasOldValues.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Traits;
+
+trait HasOldValues
+{
+    protected function getOldValue(string $fieldName, $default = null)
+    {
+        if (session()->has('errors')) {
+            $oldValue = old($fieldName);
+            
+            if ($oldValue !== null) {
+                return $oldValue;
+            }
+        }
+        
+        return $default;
+    }
+}
