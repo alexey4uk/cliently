c7e129521360d3e6a5d4ccc51df0cdaf70762aee
alex <alexey4uk@gmail.com>
2026-01-26 23:59:53 +0300

    edit migration

--- database/migrations/2025_12_15_103037_create_masters_table.php
+++ database/migrations/2025_12_15_103037_create_masters_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('masters', function (Blueprint $table) {
             $table->id();
             $table->foreignId('business_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
             $table->string('first_name');
             $table->string('last_name')->nullable();
             $table->text('description')->nullable();
