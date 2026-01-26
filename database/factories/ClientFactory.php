a6d261005727458fbf23ae5574cb7576afa25568
alex <alexey4uk@gmail.com>
2026-01-27 00:00:17 +0300

    edit client factory

--- database/factories/ClientFactory.php
+++ database/factories/ClientFactory.php
@@ -18,10 +18,9 @@ class ClientFactory extends Factory
     {
         return [
             'business_id' => 1,
-            'first_name' => fake()->name(),
-            'last_name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
-            'phone' => fake()->e164PhoneNumber(),
         ];
     }
 }
