ff489be1833831b24b2eb9ec02260ec5b236de95
alex <alexey4uk@gmail.com>
2026-03-19 06:20:18 +0300

    feat: fixed a bug with recording confirmation if the source is not telegram. Added sending a link after recording confirmation.

---
+++ app/Services/Appointment/AppointmentService.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Services\Appointment;
+
+use App\Repositories\AppointmentRepositoryInterface;
+
+class AppointmentService
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct(
+        protected AppointmentRepositoryInterface $appointmentRepository
+        ){}
+
+    public function makeLink(int $appointmentId)
+    {
+        $appointment = $this->appointmentRepository->findOrFail($appointmentId);
+
+        return env('APP_URL') . "/a/{$appointment->token}";
+    }
+}
